1c7900bf6302cfe912506ace1430f77267155fe2
Matthew Johnston <warmwaffles@gmail.com>
2017-10-28 00:24:39 -0500

    Replace to_char_list with to_charlist

--- lib/slugger.ex
+++ lib/slugger.ex
@@ -18,21 +18,21 @@ defmodule Slugger do
 
   @doc """
   Return a string in form of a slug for a given string.
-  
+
   ## Examples
-      
+
       iex> Slugger.slugify(" Hi # there ")
       "Hi-there"
-      
+
       iex> Slugger.slugify("Über den Wölkchen draußen im Tore")
       "Ueber-den-Woelkchen-draussen-im-Tore"
-      
+
       iex> Slugger.slugify("Wikipedia Style", ?_)
       "Wikipedia_Style"
-      
+
       iex> Slugger.slugify("_Trimming_and___Removing_inside___")
       "Trimming-and-Removing-inside"
-    
+
   """
   @spec slugify(text :: any, separator :: char) :: String.t
   def slugify(text, separator \\ @separator_char) do
@@ -40,24 +40,24 @@ defmodule Slugger do
     |> replace_special_chars
     |> remove_unwanted_chars(separator, ~r/([^A-Za-z0-9])+/)
   end
-  
+
   @doc """
   Return a string in form of a lowercase slug for a given string.
-  
+
   ## Examples
-      
+
       iex> Slugger.slugify_downcase(" Hi # there ")
       "hi-there"
-      
+
       iex> Slugger.slugify_downcase("Über den Wölkchen draußen im Tore")
       "ueber-den-woelkchen-draussen-im-tore"
-      
+
       iex> Slugger.slugify_downcase("Wikipedia Style", ?_)
       "wikipedia_style"
-      
+
       iex> Slugger.slugify_downcase("_trimming_and___removing_inside___")
       "trimming-and-removing-inside"
-      
+
   """
   @spec slugify_downcase(text :: any, separator :: char) :: String.t
   def slugify_downcase(text, separator \\ @separator_char) do
@@ -66,23 +66,23 @@ defmodule Slugger do
     |> String.downcase
     |> remove_unwanted_chars(separator, ~r/([^a-z0-9])+/)
   end
-  
+
   @spec remove_unwanted_chars(text :: String.t, separator :: char, pattern :: Regex.t) :: String.t
   defp remove_unwanted_chars(text, separator, pattern) do
     text
-    |> String.replace(pattern, to_string([separator])) 
+    |> String.replace(pattern, to_string([separator]))
     |> String.strip(separator)
   end
-  
+
   @spec replace_special_chars(text :: any) :: String.t
   defp replace_special_chars(text) do
-    text |> to_char_list |> replace_chars |> to_string
+    text |> to_charlist |> replace_chars |> to_string
   end
-  
-  #-- Generated function `replace_chars` below --- 
-  
+
+  #-- Generated function `replace_chars` below ---
+
   # Generate replacement functions using pattern matching.
-  @spec replace_chars(char_list) :: char_list
+  @spec replace_chars(charlist) :: charlist
   {replacements, _} = Code.eval_file(@replacement_file)
 
   replacements_by_search =  replacements
@@ -129,7 +129,7 @@ defmodule Slugger do
   def truncate_slug(slug, max_length, options) do
     options = Keyword.merge(@truncation_defaults, options)
     slug
-    |> to_char_list
+    |> to_charlist
     |> truncate_charlist(max_length, {options[:hard], options[:separator]})
     |> to_string
   end
